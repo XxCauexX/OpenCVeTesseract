742198647083167a863f092af34b600bdff765c6
Cauê de Andrade <caue.andrade.505@gmail.com>
2023-06-29 17:25:59 -0300

    att

--- reconhecimentoFacial/aula.py
+++ reconhecimentoFacial/aula.py
@@ -2,12 +2,56 @@ import cv2 # biblioteca do openCV
 import numpy as np
 
 print(cv2.__version__)
-
-imagem = cv2.imread("person.png")
+#Detecção de faces com Haar cascade (OpenCV)
+imagem = cv2.imread("face/person.png")
 print(imagem.shape)
-m = [1,2,3,4,5,6,7,8,9,0,9,8,7,6,5,4,3,2,1,3,4,5,6,7,7,8,9,7,6,5,4,3,4,56,2,7,8,3,]
-for c in m :
-    c+= 1
-    cv2.imshow("teste",imagem)
-    print("imagem")
+while True:
+    cv2.imshow("Imagem colorida",imagem)
 
+    if cv2.waitKey(5) == 27:
+        break
+
+imagem_cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+print(imagem_cinza.shape)
+
+while True:
+    cv2.imshow("Imagem convertida para cinza", imagem_cinza) #cv2.imshow("Titulo",imagem) Função para exibir a imagem
+    if cv2.waitKey(5) == 27:
+        break
+
+detector_facial = cv2.CascadeClassifier('detection_recogntion/haarcascade_frontalface_default.xml')
+deteccoes = detector_facial.detectMultiScale(imagem_cinza)
+print(deteccoes) # os primeiros parametros são o eixo x e y, e os dois
+                 #ultimos são as dimenções ou o tamanho da face detectada
+
+qtdFaces = len(deteccoes)
+print(qtdFaces)
+for (x,y,w,h,) in deteccoes:
+    print(x,y,w,h)
+    cv2.rectangle(imagem, (x,y), (x + w, y + h), (0, 255, 255), 2)
+while True:
+    cv2.imshow("Face reconhecida",imagem)
+    if cv2.waitKey(5) == 27:
+        break
+
+#Redimensionamento da imagem
+imagem1 = cv2.imread("face/people1.jpg")
+print(imagem1.shape)
+imagem1_cinza = cv2.cvtColor(imagem1, cv2.COLOR_BGR2GRAY)
+print(imagem1_cinza.shape)
+
+while True:
+    cv2.imshow("Pessoas cinza",imagem1_cinza)
+    if cv2.waitKey(5) == 27:
+        break
+
+novaDeteccoes = detector_facial.detectMultiScale(imagem1_cinza)
+print(novaDeteccoes)
+print(len(novaDeteccoes))
+for (x,y,w,h) in novaDeteccoes:
+    cv2.rectangle(imagem1, (x,y), (x + w, y +h), (0, 255, 255), 2)
+while True:
+    cv2.imshow("Faces reconhecidas", imagem1)
+    if cv2.waitKey(5) == 27:
+        break
+#para corrigir o problema de falsos positivos podemos reduzir a dimensionalidade da imagem
